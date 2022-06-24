3428408cc22cbdde79eedd55b741b50a23aa25d7
iamrithu <ritimahesh29@gmail.com>
2022-06-24 20:29:17 +0530

    buttonAnimation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
+import {
+  AiOutlineReload,
+  AiOutlineCheck,
+  AiOutlineClose,
+} from "react-icons/ai";
 
 function App() {
+  const [isLoading, setLoading] = useState("true");
+  const [succeeded, setSucceeded] = useState("");
+
+  const loadingChange = () => {
+    setSucceeded("");
+    setLoading("false");
+    axios
+      .get("https://reqres.in/api/user")
+      .then((res) => {
+        if (res.status === 201 || 200) {
+          setTimeout(() => {
+            setLoading("");
+            setSucceeded("success");
+          }, 2000);
+        }
+        console.log(res.status);
+      })
+      .catch((res) => {
+        setTimeout(() => {
+          setLoading("");
+
+          setSucceeded("failed");
+
+          setTimeout(() => {
+            setLoading("true");
+            setSucceeded("");
+          }, 2000);
+        }, 2000);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={loadingChange}
+        className="btn"
+        disabled={succeeded === "success"}
+      >
+        {isLoading === "false" && <AiOutlineReload className="hello" />}
+        {isLoading === "true" && "click"}
+        {succeeded === "success" && <AiOutlineCheck className="success" />}
+        {succeeded === "failed" && <AiOutlineClose className="failed" />}
+      </button>
     </div>
   );
 }
